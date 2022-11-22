d33ffc7f295c93e2c1e60a1884f34da6b5d62bc4
Drogolova <drogolova1997@mail.ru>
2022-11-23 00:21:25 +0300

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        Author author1 = new Author("Leo", "Tolstoy");
+        Author author2 = new Author("Fyodor", "Dostoevsky");
+        Book book1 = new Book("War and peace", author1, 1870);
+        Book book2 = new Book("Crime and punishment", author2, 1866);
+        book1.setPublicationYear(1869);
     }
 }
